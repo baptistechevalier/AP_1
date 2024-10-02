065d9cb7e6d971a28c52c8176407cdde83b00a65
baptistechevalier <151515603+baptistechevalier@users.noreply.github.com>
2024-10-02 13:18:28 +0200

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db="arrasgame";
-$dbhost="localhost";
-$dbport=3306;
-$dbuser="root";
-$dbpasswd="root";
-
-$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-$pdo->exec("SET CHARACTER SET utf8");
